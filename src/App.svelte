fb35d307aeaa3b6d77f67ad409b5bd70be128d61
Yann Klein <yann.klein@me.com>
2021-09-10 19:56:55 +0900

    init svelte app with hero and component file backbone for new Yannuikit

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import SideBar from "./SideBar.svelte";
+	import Hero from "./Hero.svelte";
+	import Section from "./Section.svelte";
+	const sections = [
+		'Browser killer',
+		'Responsive container',
+		'Page frame',
+		'Cards',
+		'Item cards',
+		'Navbar',
+	]
+	const title="Yann's UI Kit ❐";
+	const description="A repo of interesting non-bootstrap UI components ready to use.";
+</script>
+
+<Hero title={title} description={description}/>
+<div class="page-container">
+	<SideBar sections={sections}/>
+	<div class="content-container">
+		{#each sections as section}
+			<Section name={section}/>
+		{/each}
+	</div>
+</div>
+
+<style>
+.page-container {
+  display: flex;
+}
+
+.content-container {
+  flex-grow: 1;
+  margin: 0 auto;
+  padding: 16px;
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media (max-width: 992px) {
+  .content-container {
+    width: 650px;
+  }
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media (max-width: 768px) {
+  .content-container {
+    width: 500px;
+  }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media (max-width: 576px) {
+  .content-container {
+    width: 300px;
+  }
+}
+
+</style>
---
+++ src/Hero.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let title, description;
+</script>
+
+
+<div class="hero">
+  <div class="content-container">
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
+</div>
+
+<style>
+  .hero {
+    padding: 80px 0;
+    background: -webkit-linear-gradient( -70deg,  rgba(9,9,121,0.6) 11.2%, rgba(144,6,161,0.6) 53.7%, rgba(0,212,255,0.6) 100.2% );  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient( -70deg,  rgba(9,9,121,0.6) 11.2%, rgba(144,6,161,0.6) 53.7%, rgba(0,212,255,0.6) 100.2% ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+
+  .content-container {
+    flex-grow: 1;
+    margin: 0 auto;
+    padding: 16px;
+  }
+
+  /* Large devices (laptops/desktops, 992px and up) */
+  @media (max-width: 992px) {
+    .content-container {
+      width: 650px;
+    }
+  }
+
+  /* Medium devices (landscape tablets, 768px and up) */
+  @media (max-width: 768px) {
+    .content-container {
+      width: 500px;
+    }
+  }
+
+  /* Small devices (portrait tablets and large phones, 600px and up) */
+  @media (max-width: 576px) {
+    .content-container {
+      width: 300px;
+    }
+    .hero {
+      padding: 56px 0;
+    }
+    .hero p {
+      margin: 0;
+    }
+  }
+
+</style>
